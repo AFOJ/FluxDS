4fb29a427e746fdbd7c198f12331545fd0d02da4
OJ Abba <phavorabba@gmail.com>
2026-01-20 00:02:31 +0000

    Add link to Storybook

--- src/app.tsx
+++ src/app.tsx
@@ -1,28 +1,14 @@
-import { Card } from './components/card/Card'
-
 export function App() {
   return (
-    <div class={'p-6 space-y-8'}>
-      <section>
-        <h2>Cards</h2>
-        <div class={'flex gap-4 flex-wrap'}>
-          <Card
-            mainText="Join the family."
-            ctaText="Become a member"
-            onCtaClick={() => {}}
-            icon="user"
-          />
-
-          <span data-theme="BrandB">
-            <Card
-              mainText="Join the family."
-              ctaText="Become a member"
-              onCtaClick={() => {}}
-              icon="user"
-            />
-          </span>
-        </div>
-      </section>
+    <div class={'p-6 space-y-3'}>
+      <h1 class="text-3xl font-bold">Hello 👋</h1>
+      <a
+        class={'text-teal-600 underline'}
+        href={'https://oj-biglight-storybook.netlify.app/'}
+        target={"_blank"}
+      >
+        View Storybook here
+      </a>
     </div>
   )
 }
